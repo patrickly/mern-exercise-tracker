5f0f7b3f0cb587e960451a81eaeea4206e08cdd6
Patrick Ly <patrickkimly@gmail.com>
2019-09-22 22:45:43 -0700

    create-exercise component post request to backend
    - 1:29:17

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css"
 
@@ -23,10 +24,15 @@ export default class CreateExercise extends Component {
      }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+          .then(response => {
+            if(response.data.length > 0){
+              this.setState({
+                users: response.data.map(user => user.username),
+                username: response.data[0].username
+              });
+            }
+          });
     }
     
     onChangeUsername(e) {
@@ -63,7 +69,8 @@ export default class CreateExercise extends Component {
             date: this.state.date
         };
 
-        console.log(exercise);
+        axios.post('http://localhost:5000/exercises/add', exercise)
+        .then(res => console.log(res.data));
 
         window.location = '/';
     }
